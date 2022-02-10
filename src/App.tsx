dcaad1fc41d18aaa78b1b38df0574fe1fc7d676a
Aashish Panchal <aipanchal51@gmail.com>
2022-02-11 00:56:32 +0530

    day 1 completed......

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
+import Footer from "./components/Footer";
+import AppRouter from "./router";
 
 export default function App() {
+  const [darkTheme, setDarkTheme] = React.useState(true);
+
+  const getTheme = () => {
+    return darkTheme ? "dark" : "";
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Hello, world!</h1>
+    <div className={`${getTheme()}`}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <AppRouter />
+        <Footer />
+      </div>
     </div>
   );
 }
